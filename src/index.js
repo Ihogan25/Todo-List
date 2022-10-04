52206671a39a4e74bee68610ec706b949d04e6e2
Isaiah Hogan <isaiah.hogan25@gmail.com>
2022-10-04 11:55:37 -0500

    basic UI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('hi')
+import './style.css';
+
+const newProjectBtn = document.getElementById('new-project');
+const projects = [];
+
+
+
+class Project {
+    constructor(title) {
+        this.title = title,
+        this.tasks = [];
+
+    }
+}
+
+
+
+
+
+
+
+
+
+newProjectBtn.addEventListener('click', function(){
+    let projectForm = document.getElementById('project-form');
+    projectForm.style.display = 'block'
+})
