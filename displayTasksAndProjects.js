0eaddd28d0ddb0b3aac0f26d8257259db6c4bc15
Isaiah Hogan <isaiah.hogan25@gmail.com>
2022-10-06 16:40:50 -0500

    adding moreimported modules and stylesheets

---
+++ displayTasksAndProjects.js
@@ -0,0 +1,20 @@
+// import projects from "../src/index.js";
+
+
+// const display = (()=> {
+
+//     const pageData = {
+//         mainTitle: document.getElementById('main-title'),
+//         projectsList: document.getElementById('projects-list'),
+//         tasksList: document.getElementById('tasks-list')
+//     }
+
+//     const selectedProject=()=> {
+
+//     }
+    
+
+//     return {
+        
+//     }
+// })();
