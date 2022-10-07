1793c0a5e0c974f4a2c0292294f36293beafa62a
Isaiah Hogan <isaiah.hogan25@gmail.com>
2022-10-07 17:48:15 -0500

    created add project functionality and add a task to a project and display selected project and its tasks

--- displayTasksAndProjects.js
+++ displayTasksAndProjects.js
@@ -1,20 +1,43 @@
-// import projects from "../src/index.js";
+import projects from "../src/index.js";
+import createProject from '../dist/createProject.js';
 
 
-// const display = (()=> {
+const display = (()=> {
+    const pageData = {
+        mainTitle: document.getElementById('main-title'),
+        projectsList: document.getElementById('projects-list'),
+        tasksList: document.getElementById('tasks-list')
+    }
 
-//     const pageData = {
-//         mainTitle: document.getElementById('main-title'),
-//         projectsList: document.getElementById('projects-list'),
-//         tasksList: document.getElementById('tasks-list')
-//     }
 
-//     const selectedProject=()=> {
+    const showProjectContent =(ev)=> {
+        ev.preventDefault();
+        createProject.domProjs.forEach( projCard => {
+            projCard.addEventListener('click', ()=> {
+                pageData.mainTitle.textContent = projCard.textContent;
+                display.showProjTasks();  
+            })
+        })
+    }
 
-//     }
-    
+    const showProjTasks =()=> {
+        for(let i = 0; i < projects.length; i++) {
+            if(pageData.mainTitle.textContent === projects[i].title) {
+                while (pageData.tasksList.hasChildNodes()) {
+               pageData.tasksList.removeChild(pageData.tasksList.firstChild)
+               }
+               for(let j = 0; j < projects[i].tasks.length; j++) {
+                pageData.tasksList.appendChild(projects[i].tasks[j])
+               }
 
-//     return {
-        
-//     }
-// })();
+            }
+        }
+    }
+    return {
+        showProjectContent,
+        showProjTasks,
+        pageData
+    }
+})();   
+
+export default display;
