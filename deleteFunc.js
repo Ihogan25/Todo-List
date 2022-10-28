95cf91c1d982238f4f14ffde059596720e1a5821
Isaiah Hogan <isaiah.hogan25@gmail.com>
2022-10-28 15:29:40 -0500

    finished sorting and displaying tasks by days and importance

--- deleteFunc.js
+++ deleteFunc.js
@@ -3,50 +3,76 @@ import createProject from "./createProject.js";
 import {display} from "./displayTasksAndProjects.js";
 import  {selectedProj} from './displayTasksAndProjects.js';
 import { pageData } from "./displayTasksAndProjects.js";
+import task from "./addTask.js";
+import _ from "lodash";
+
 
 const dltObj = (()=> {
-    const projObj =(ev)=> {
-        ev.preventDefault();
+    
+    //delete project func
+    const projObj =()=> {
         //queries for all project buttons on the page
         let projDltBtns = document.querySelectorAll('.delete-project-btn');
         //for each delete project button add an event listener of click
         projDltBtns.forEach(btn => {
-            btn.addEventListener('click', ()=> {
-                if(createProject.domProjs.includes(btn.parentElement)) {
-                pageData.projectsList.removeChild(pageData.projectsList.children[createProject.domProjs.indexOf(btn.parentElement)]);
-                createProject.domProjs.splice(createProject.domProjs.indexOf(btn.parentElement),1);
-                projects.splice(createProject.domProjs.indexOf(btn.parentElement),1); 
-                if(projects.length === 0) {
-                    // pageData.mainTitle.textContent = 'Create a Project!';
-                    selectedProj.chngProj({})
-                    pageData.mainTitle.textContent = 'Create a Project!';
-                    for(let i = 0; i < pageData.tasksList.childNodes.length; i++) {
-                        pageData.tasksList.removeChild(pageData.tasksList.firstChild);
-                    }                
-                }
-                else if(projects.length > 0){
-                    selectedProj.chngProj(projects[Math.floor(Math.random() * pageData.projectsList.childNodes.length)])
-                    console.log(selectedProj.proj)
-                    pageData.mainTitle.textContent = selectedProj.proj.title;
-                    display.showProjTasks()
-                    
-                }
-                }
-            })
-        })
-    }
+            btn.addEventListener('click', (ev)=> {
+                    ev.preventDefault()
+                    let indx = createProject.domProjs.indexOf(btn.parentElement);
+                    if(createProject.domProjs.includes(btn.parentElement)) {
+                        for(let i = 0; i < task.allTasks.length; i++) {
+                            if(_.isEqual(task.allTasks[i],projects[indx].tasks[i]) ) {
+                                task.allTasks.splice(i,1);
+                            }
+                            else {
+                                console.log(task.allTasks[i], projects[indx].tasks[i]);
+                            }
+                        }
+                        pageData.projectsList.removeChild(pageData.projectsList.children[indx]);
+                        createProject.domProjs.splice(indx,1);
+                        projects.splice(indx,1);
+                        if(projects.length > 0 && selectedProj.proj !== projects[indx]) {
+                           selectedProj.chngProj(projects[0]);
+                           pageData.mainTitle.textContent = selectedProj.proj.title;
+                           display.showProjTasks();
+                        }
+                        else if(projects.length > 0 && selectedProj.proj === projects[indx]){
+                            selectedProj.chngProj(projects[0]);
+                            pageData.mainTitle.textContent = selectedProj.proj.title;
+                            display.showProjTasks();
+                        }
+                        else{
+                            selectedProj.chngProj({});
 
+                            pageData.mainTitle.textContent = 'Create a Project!';
+                            while (pageData.tasksList.hasChildNodes()) {
+                            pageData.tasksList.removeChild(pageData.tasksList.firstChild)
+                         }
+                        }
+                            
+                    }
+        })})
+    }
+    //delete tasks func
     const taskObj=()=> {
         //queries for all current delete buttons on page for the selected project
-        let selectedProjDelBtn = document.querySelectorAll('.del-task-btn');
-        //for each of those buttons add an event listener to each of them so when clicked it deletes its parent element which is a task in the selected 
-        selectedProjDelBtn.forEach(btn => {
+        let dtlTskBtns = document.querySelectorAll('.del-task-btn');
+        // for each of those buttons add an event listener to each of them so when clicked it deletes its parent element which is a task in the selected 
+        dtlTskBtns.forEach(btn => {
             btn.addEventListener('click', ()=> {
                 //looping through each project to find the project in which the task belongs to
-                if(selectedProj.proj.tasks.includes(btn.parentElement)) {
-                    selectedProj.proj.rmTask(selectedProj.proj.tasks.indexOf(btn.parentElement));
+                for(let i = 0; i < selectedProj.proj.tasks.length; i++) {
+                    if(selectedProj.proj.tasks[i].taskCard === btn.parentElement) {
+                        let taskIndx = selectedProj.proj.tasks.indexOf(selectedProj.proj.tasks[i])
+                        selectedProj.proj.rmTask(taskIndx);
+                        pageData.tasksList.removeChild(pageData.tasksList.children[taskIndx]);
+                        for(let i = 0; i < task.allTasks.length; i++) {
+                            if(task.allTasks[i].taskCard === btn.parentElement) {
+                                task.allTasks.splice(i, 1);
+                            }
+                    }
                 }
-                display.showProjTasks();
+        
+                }
             })
         })
     }   
--- displayTasksAndProjects.js
+++ displayTasksAndProjects.js
@@ -1,6 +1,7 @@
 import projects from "../src/index.js";
 import createProject from '../dist/createProject.js';
 import dltObj from "./deleteFunc.js";
+import _ from 'lodash';
 
 let selectedProj = {
     proj: {
@@ -16,36 +17,47 @@ let pageData = {
     projectsList: document.getElementById('projects-list'),
     tasksList: document.getElementById('tasks-list'),
     projTitle: document.getElementById('project-title'),
-    taskTitle: document.getElementById('task-title')
+    taskTitle: document.getElementById('task-title'),
+    taskDate: document.getElementById('task-date'),
+    taskImportance: document.getElementById('task-importance')
 }
 
 const display = (()=> { 
     //this function displays the slected projects tasks and changed the main header to the selected projects title
-    const showProjectContent =(ev)=> {
-        ev.preventDefault();
+    const showProjectContent =()=> {
         createProject.domProjs.forEach( projCard => {
-            projCard.addEventListener('click', ()=> {
-                if(projects.length > 0) {
-                    selectedProj.chngProj(projects[createProject.domProjs.indexOf(projCard)]);
-                     pageData.mainTitle.textContent = selectedProj.proj.title;
-                     display.showProjTasks(); 
+            projCard.addEventListener('click', (ev)=> {
+                ev.preventDefault()
+                let projIndx = createProject.domProjs.indexOf(projCard);
+                
+                if(_.isEqual(selectedProj.proj, projects[projIndx]) === false) {
+                    selectedProj.chngProj(projects[projIndx]);
+                    pageData.mainTitle.textContent = selectedProj.proj.title;
+                    showProjTasks();
                 }
+                else if(_.isEqual(selectedProj.proj, projects[projIndx]) === true) {
+                    selectedProj.chngProj(projects[projIndx]);
+                    pageData.mainTitle.textContent = selectedProj.proj.title;
+                    showProjTasks();
+                }
+
+                
+              
             })
         })
     }
 
+
     //this function is a child function to the showProject content, this shows the slected projects tasks
     const showProjTasks =()=> {
-        if(projects.length > 0) {
             while (pageData.tasksList.hasChildNodes()) {
                 pageData.tasksList.removeChild(pageData.tasksList.firstChild)
                 }
             for(let i = 0; i < selectedProj.proj.tasks.length; i++) {
-                console.log(selectedProj.proj)
-                pageData.tasksList.appendChild(selectedProj.proj.tasks[i])
+                pageData.tasksList.appendChild(selectedProj.proj.tasks[i].taskCard)
             }
             
-        }
+        
     }
 
     //returns all functions4444
